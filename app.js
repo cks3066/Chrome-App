5592957c4dd53fcacf26e69a72bd089865b72564
On <cks3066@naver.com>
2021-09-05 02:10:00 +0900

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+let a = 10;
+const b = 2;
+const myName = "nico";
+const veryLongVariableName = 0;
+
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
